c9532d488d25909f26dcf52b10c4e544bf593315
schlopp96 <71921821+schlopp96@users.noreply.github.com>
2022-07-17 00:18:16 -0500

    Implemented logging system
    Signed-off-by: schlopp96 <71921821+schlopp96@users.noreply.github.com>

---
+++ ChooseMyGame/appLogger/logger.py
@@ -0,0 +1,150 @@
+import logging
+
+
+class _LogGenerator():
+    """Wrapper for application logging.
+
+    - Uses built-in Python `logging` module.
+
+    - Class methods:
+        - :func:`debug(self, msg) -> None`
+            - Logs a message with level `DEBUG`.
+
+        - :func:`info(self, msg) -> None`
+            - Logs a message with level `INFO`.
+
+        - :func:`warning(self, msg) -> None`
+            - Logs a message with level `WARNING`.
+
+        - :func:`error(self, msg) -> None`
+            - Logs a message with level `ERROR`.
+
+        - :func:`critical(self, msg) -> None`
+            - Logs a message with level `CRITICAL`.
+    """
+
+    CRITICAL = 50
+    FATAL = CRITICAL
+    ERROR = 40
+    WARNING = 30
+    WARN = WARNING
+    INFO = 20
+    DEBUG = 10
+    NOTSET = 0
+
+    def __init__(
+            self,
+            name: str,
+            log_file: str,
+            log_format: str = '[%(asctime)s - %(levelname)s] : %(message)s\n',
+            datefmt: str = "%Y-%m-%d %H:%M:%S",
+            level: int = INFO,
+            stream: bool = False):
+        """Initialize logger instance.
+
+        - For the :param:`log_lvl` parameter, the level of logging can be any of the following:
+            - CRITICAL = 50
+            - FATAL = CRITICAL
+            - ERROR = 40
+            - WARNING = 30
+            - WARN = WARNING
+            - INFO = 20
+            - DEBUG = 10
+            - NOTSET = 0
+
+        ---
+
+        :param name: Name of logger.
+        :type name: :class:`str`
+        :param log_file: file to write log entries.
+        :type log_file: :class:`str`
+        :param log_fmt: initialize log formatter either with the specified format string, or a default as described above, defaults to '[%(asctime)s - %(levelname)s] : %(message)s'
+        :type log_fmt: :class:`str`, optional
+        :param date_fmt: set date formatting, defaults to "%Y-%m-%d %H:%M:%S"
+        :type date_fmt: :class:`str`, optional
+        :param level: Set the logging level of this logger. Level must be an int or a str, defaults to `INFO`.
+        :type level: :class:`int`, optional
+        :param stream: If True, toggle streaming to stdout.
+        :type stream: :class:`bool`, optional
+        :return: logging class instance
+        :rtype: None
+        """
+
+        self.name = name
+        self.logger = logging.getLogger(name)
+        self.log_format = log_format
+        self.datefmt = datefmt
+        self.level = level
+        self.formatter = logging.Formatter(log_format, datefmt=datefmt)
+        self.log_file = log_file
+        self.fhandler = logging.FileHandler(log_file)
+        self.logger.addHandler(self.fhandler)
+        self.fhandler.setFormatter(self.formatter)
+        self.logger.setLevel(level)
+        self.stream = stream
+
+        if stream:  # if stream is True, then set logging stream to stdout
+            self.logger.addHandler(logging.StreamHandler())
+
+    def debug(self, msg) -> None:
+        """Logs a message with level `DEBUG`.
+
+        ---
+
+        :param msg: message to be logged.
+        :type msg: :class:`str`
+        :return: creates log entry.
+        :rtype: None
+        """
+        return self.logger.debug(msg)
+
+    def info(self, msg):
+        """Logs a message with level `INFO`.
+
+        ---
+
+        :param msg: message to be logged.
+        :type msg: :class:`str`
+        :return: creates log entry.
+        :rtype: None
+        """
+        return self.logger.info(msg)
+
+    def warning(self, msg):
+        """Logs a message with level `WARNING`.
+
+        ---
+
+        :param msg: message to be logged.
+        :type msg: :class:`str`
+        :return: creates log entry.
+        :rtype: None
+        """
+        return self.logger.warning(msg)
+
+    def error(self, msg):
+        """Logs a message with level `ERROR`.
+
+        ---
+
+        :param msg: message to be logged.
+        :type msg: :class:`str`
+        :return: creates log entry.
+        :rtype: None
+        """
+        return self.logger.error(msg)
+
+    def critical(self, msg):
+        """Logs a message with level `CRITICAL`.
+
+        ---
+
+        :param msg: message to be logged.
+        :type msg: :class:`str`
+        :return: creates log entry.
+        :rtype: None
+        """
+        return self.logger.critical(msg)
+
+
+logger = _LogGenerator('ChooseMyGame', 'logs/ChooseMyGameLog.log')
--- ChooseMyGame/main.py
+++ ChooseMyGame/main.py
@@ -3,29 +3,45 @@
 import sys
 from os import chdir
 from os.path import dirname
+from typing import NoReturn
 
 sys.path.insert(0, dirname(
     dirname(__file__)))  # Ensure module can be found by Python.
 
 chdir(dirname(__file__))  # Change working directory to main module.
 
-from ChooseMyGame.appGUI.gui import window, sg
+from ChooseMyGame.appGUI.gui import sg, window
+from ChooseMyGame.appLogger.logger import logger
 
 __version__ = '0.1.0'
 
+textborder: str = f'\n<{"*" * 121}>\n'  # Text border.
 
-def activate_GUI():
+
+def activate_GUI() -> None:
     while True:
-        event, values = window.read()
+        event, values = window.read()  # Read GUI event.
+
+        print(event, values)  # Enable for easier debugging
 
         if event in [sg.WIN_CLOSED, sg.WIN_X_EVENT, 'Exit']:
             break
 
-    window.close()  # Close the window and return resources to the OS
+    window.close()  # Close the window and return resources to the OS.
 
 
-def main():
-    activate_GUI()
+def main() -> None:
+    logger.info(f'Started ChooseMyGame v{__version__}...')
+
+    activate_GUI()  # Start program window.
+
+    return program_exit(0)  # Exit program.
+
+
+def program_exit(code: int) -> NoReturn | None:
+    logger.info(f'Exiting program...\n{textborder}')
+
+    return sys.exit(code)  # Exit program.
 
 
 if __name__ == "__main__":
